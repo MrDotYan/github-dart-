97e2aa6139a67870ee76c1621ae88d0e6046a672
mrdotyan <2564217790@qq.com>
2021-03-03 09:55:09 +0800

    init project and 1.0version completed !

--- push.cmd
+++ push.cmd
@@ -1,1 +1,1 @@
-git init && git add -A && git commit -m "init project and 1.0version completed !"&& git remote add origin https://github.com/MrDotYan/github-dart-.git && git push orgin main
+ git add -A && git commit -m "init project and 1.0version completed !"&&  git push origin main
